33ecf65f0f89632f464bb5e68922f750f802e2eb
Vladislav Spirin <vspirin.qa@gmail.com>
2017-12-28 16:00:56 +0300

    accordions one-liner

--- src/sjs/modules/accordions.js
+++ src/sjs/modules/accordions.js
@@ -5,12 +5,7 @@ var accordions = (function($) {
     return {
         init: function () {
             $(document).on('click', '.js-accordion', function () {
-                if ($(this).parent().hasClass('accordion_opened')) {
-                    $(this).parent().find('.accordion__content').slideUp(200);
-                } else {
-                    $(this).parent().find('.accordion__content').slideDown(200);
-                }
-                $(this).parent().toggleClass('accordion_opened');
+                $(this).parent().toggleClass('accordion_opened').find('.accordion__content').slideToggle(200);
             });
         }
     }
